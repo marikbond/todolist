42a2bd2700040f5b5ea9a26a82b9c1e7b3130a0f
Mark <markb.privat@gmail.com>
2017-12-09 10:08:21 -0500

    receiving status list from data base

---
+++ app/models/statuses.js
@@ -0,0 +1,27 @@
+var connectionPool = require('./index');
+
+var statusAPI = {
+    findAll: function (callback) {
+        connectionPool.getConnection(function(err, connection) {
+            if (err) {
+                console.error('ERROR connecting!: ' + err.stack);
+                callback(err);
+                return;
+            }
+            var sql = 'SELECT * FROM statuses;';
+            connection.query(sql, function (error, results) {
+                connection.release();
+                if (error) throw error;
+                callback(null, extractStatuses(results));
+            });
+        });
+    }
+};
+
+function extractStatuses(results) {
+    return results.map(function (row) {
+        return row.status;
+    })
+}
+
+module.exports = statusAPI;
